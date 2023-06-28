c68f5012bef6447fc2ffb403ed5863e7e075c828
ChangJin-Lee <ckdwls525@gmail.com>
2023-06-28 23:43:55 +0900

    init Fastlane Project

---
+++ iOS-Fastlane-test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-Fastlane-test
+//
+//  Created by CHANG JIN LEE on 2023/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Fastlane-test/iOS_Fastlane_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Fastlane_testApp.swift
+//  iOS-Fastlane-test
+//
+//  Created by CHANG JIN LEE on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Fastlane_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
